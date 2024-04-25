cb8da532b6cd540d5a3a5dae76ba95606dbc1d9c
M.Sato <M.Sato@LAPTOP-KILGTHC3>
2024-04-25 11:22:47 +0900

    DAO更新

--- src/main/java/dao/ClassNumDAO.java
+++ src/main/java/dao/ClassNumDAO.java
@@ -1,11 +1,32 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import bean.School;
 
 public class ClassNumDAO extends DAO{
-	public List<String> filter(School school) {
-		
+	public List<String> filter(School school) throws Exception{
+		List<String> list=new ArrayList<>();
+
+		Connection con=getConnection();
+
+		PreparedStatement st=con.prepareStatement(
+			"select * from school where cd = ?");
+		st.setString(1, school.toString());
+		ResultSet rs=st.executeQuery();
+
+		while (rs.next()) {
+			list.add(rs.getString("name"));
+		}
+
+		st.close();
+		con.close();
+
+		return list;
+
 	}
 }
