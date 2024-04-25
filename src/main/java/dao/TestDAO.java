e9d72647ee4f8397d66e2f70a2c7512fa792a94d
M.Sato <M.Sato@LAPTOP-KILGTHC3>
2024-04-25 11:02:31 +0900

    DAOの更新

--- src/main/java/dao/TestDAO.java
+++ src/main/java/dao/TestDAO.java
@@ -3,6 +3,7 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import bean.School;
@@ -41,8 +42,36 @@ public class TestDAO extends DAO{
 	private List<Test> postFilter(ResultSet rSet,School school){
 		
 	}
-	public List<Test> filter(int entYear,String classNum,Subject subject,int num, School school){
-		
+	public List<Test> filter(int entYear,String classNum,Subject subject,int no, School school) throws Exception{
+		List<Test> list=new ArrayList<>();
+
+		Connection con=getConnection();
+
+		PreparedStatement st=con.prepareStatement(
+			"select * from student where ent_year = ? and class_num = ? and subject_cd = ? no = ? and school_cd = ?");
+		st.setInt(1, entYear);
+		st.setString(2, classNum);
+		st.setString(3, subject.toString());
+		st.setInt(4, no);
+		st.setString(5, school.toString());
+		ResultSet rs=st.executeQuery();
+
+		while (rs.next()) {
+			Test test=new Test();
+			test.setNo(no);
+			test.setStudent((Student)rs.getObject("stuent_no"));
+			test.setSubject(subject);
+			test.setSchool(school);
+			test.setClassNum(classNum);
+			test.setPoint(rs.getInt("point"));
+			list.add(test);
+		}
+
+		st.close();
+		con.close();
+
+		return list;
+
 	}
 	
 	public boolean save(List<Test> list) {
--- src/main/java/dao/TestListStudentDAO.java
+++ src/main/java/dao/TestListStudentDAO.java
@@ -1,6 +1,9 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import bean.Student;
@@ -13,7 +16,30 @@ public class TestListStudentDAO extends DAO{
 	private List<TestListStudent> postFilter(ResultSet rSet) {
 		
 	}
-	public List<TestListStudent> folter(Student student) {
-		
+	public List<TestListStudent> filter(Student student) throws Exception{
+		List<TestListStudent> list=new ArrayList<>();
+
+		Connection con=getConnection();
+
+		PreparedStatement st=con.prepareStatement(
+			"select * from test INNER JOIN subject ON test.subject_cd = subject.cd where student_no = ?");
+		st.setString(1, student.toString());
+
+		ResultSet rs=st.executeQuery();
+
+		while (rs.next()) {
+			TestListStudent stlst=new TestListStudent();
+			stlst.setNum(rs.getInt("no"));
+			stlst.setPoint(rs.getInt("point"));
+			stlst.setSubjectCd(rs.getString("subject_cd"));
+			stlst.setSubjectName(rs.getString("name"));
+			list.add(stlst);
+		}
+
+		st.close();
+		con.close();
+
+		return list;
+
 	}
 }
